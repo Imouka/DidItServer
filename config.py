393343ec93b06d4ad2225ff2307160f1ddb0a7a3
Francois Matvienko <francois.matvienko@orange.fr>
2019-12-01 16:39:13 +0100

    First push to heroku

--- config.py
+++ config.py
@@ -10,6 +10,12 @@ import os
 SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#HLKQOSD#_1G"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 
 
