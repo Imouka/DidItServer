46eb8ffc962ec978c502a0bc9815757e7464681f
Francois Matvienko <francois.matvienko@orange.fr>
2019-12-01 16:13:14 +0100

    Setup Project

---
+++ DidItBackend/views.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Config options - Make sure you created a 'config.py' file.
+app.config.from_object('config')
+# To get one variable, tape app.config['MY_VARIABLE']
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/user/<id>/')
+def getUserById(id):
+    if(id=="1"):
+        return("I am user 1")
+    return("No user "+id)
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ run.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+from DidItBackend import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
